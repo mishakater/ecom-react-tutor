a8708ecba1974d2159c03ea6b635c3bc750eb951
mishakater <51138707+mishakater@users.noreply.github.com>
2022-03-27 20:06:36 +0300

    collection components and styles added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import HomePage from "./pages/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Routes, Route } from "react-router-dom";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='hats' element={<HatsPage />} />
+        <Route path='/shop' element={<ShopPage />} />
       </Routes>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Directory from "../components/directory/directory.component";
+import Directory from "../../components/directory/directory.component";
 
-import "./homepage.styles.scss";
+import "../homepage/homepage.styles.scss";
 
 const HomePage = () => (
   <div className='homepage'>
