7bca0bb35f5717594c52dd2d96b171aebde54227
mishakater <51138707+mishakater@users.noreply.github.com>
2022-03-27 16:37:23 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage.component";
+import { Routes, Route } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
-  }
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='hats' element={<HatsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { useNavigate, useMatch } from "react-router-dom";
 
-export const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+export const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
